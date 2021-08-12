f236987fe7c468fff3fc765dc8054a4744d21b4a
Anh-vu <anhuvdinh@gmail.com>
2021-08-12 18:25:13 -0500

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// // const totalBatteries = batteryBatches.reduce(function(x, y){
+// //     return x +y;
+// // })
+
+// function calcTotalBatteries(array){
+//     const totalBatteries = array.reduce()
+//     return totalBatteries;
+// }
+
+// calcTotalBatteries(batteryBatches);
+// console.log(totalBatteries);
+
+const totalBatteries = batteryBatches.reduce(myFunc);
+
+function myFunc(total, num) {
+  return total + num;
+}
+
+console.log(batteryBatches.reduce(myFunc));
